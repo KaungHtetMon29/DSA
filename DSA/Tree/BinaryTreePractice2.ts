07da08de3ca096d78847fa7b317b11c68d9eba63
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2025-02-20 23:51:49 +0700

    tree test2

--- DSA/Tree/BinaryTreePractice2.ts
+++ DSA/Tree/BinaryTreePractice2.ts
@@ -16,10 +16,10 @@ namespace BinaryTreeTest2 {
     public get left(): TNode | null {
       return this._left;
     }
-    public set right(node: TNode) {
+    public set right(node: TNode | null) {
       this._right = node;
     }
-    public set left(node: TNode) {
+    public set left(node: TNode | null) {
       this._left = node;
     }
   }
@@ -41,17 +41,17 @@ namespace BinaryTreeTest2 {
         let currentNode = this._root;
         let traverse = true;
         while (traverse) {
-          if (currentNode === newNode) {
+          if (currentNode.value === newNode.value) {
             traverse = false;
             return true;
-          } else if (currentNode < newNode) {
+          } else if (currentNode.value < newNode.value) {
             if (currentNode.right === null) {
               currentNode.right = newNode;
               return true;
             } else {
               currentNode = currentNode.right;
             }
-          } else if (currentNode > newNode) {
+          } else if (currentNode.value > newNode.value) {
             if (currentNode.left === null) {
               currentNode.left = newNode;
               return true;
@@ -63,10 +63,63 @@ namespace BinaryTreeTest2 {
       }
     }
     delete(value: string): TNode | null {
-      this._root = this.deleteNode(value, this._root);
+      this._root = this.deleteNodeR(value, this._root);
       return this._root;
     }
 
-    deleteNode(value: string, node: TNode | null): TNode | null {}
+    private deleteNodeR(value: string, node: TNode | null): TNode | null {
+      if (node === null) {
+        return null;
+      }
+      if (value > node.value) {
+        node.right = this.deleteNodeR(value, node.right);
+      } else if (value < node.value) {
+        node.left = this.deleteNodeR(value, node.left);
+      } else {
+        if (node.left === null) {
+          return node.right;
+        } else if (node.right === null) {
+          return node.left;
+        }
+        const rightMin = this.findMin(node.right);
+        node.value = rightMin.value;
+        node.right = this.deleteNodeR(rightMin.value, node.right);
+      }
+      return node;
+    }
+    findMin(node: TNode) {
+      while (node.left !== null) {
+        return node.left;
+      }
+      return node;
+    }
+
+    BreadFirstSearchR(list: any[], queue: TNode[]): TNode[] {
+      if (!queue.length) {
+        return list;
+      }
+      let node = queue.shift();
+      list.push(node?.value);
+      if (node?.left) {
+        queue.push(node.left);
+      }
+      if (node?.right) {
+        queue.push(node.right);
+      }
+      return this.BreadFirstSearchR(list, queue);
+    }
   }
+  const Tree = new BinaryTree();
+  console.log("Tree");
+  Tree.addNode("e");
+  Tree.addNode("a");
+  Tree.addNode("g");
+  Tree.addNode("b");
+  Tree.addNode("c");
+  Tree.addNode("d");
+  Tree.addNode("f");
+  Tree.addNode("h");
+  console.log("added Tree");
+  Tree.delete("g");
+  console.log(Tree.BreadFirstSearchR([], [Tree.root as TNode]));
 }
